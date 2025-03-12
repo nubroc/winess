2a14a03b30b9c06fdc68e87f4d876479e196921d
Matéo Journiac <journiacmateo@gmail.com>
2025-03-12 17:29:53 +0100

    commit cs

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Card = ({ image, title }) => {
+const Card = ({ image, title, numero }) => {
   return (
-    <div className="card" style={{ backgroundImage: `url(${image})` }}>
-      <h2>{title}</h2>
+    <div className={`card ${numero}`}>
+      <div className="card-image">
+        <img src={image} alt={title} />
+      </div>
+      <div className="card-content">
+        <h2>{title}</h2>
+      </div>
     </div>
   );
 };
